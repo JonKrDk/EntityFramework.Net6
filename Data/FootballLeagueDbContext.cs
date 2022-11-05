1db1e61a29b4af8e2fa4a1eef66d867d20e9fbbe
Jon Kristensen <jon@jonkr.dk>
2022-11-05 10:59:43 +0100

    Initial Migrations

---
+++ Data/FootballLeagueDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class FootballLeagueDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FootballLeagueDb");
+        }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<League> Leagues { get; set; }
+    }
+}
+ 
