38151f6f2a9a5ada1e36b188957c475d986c72ec
Jon Kristensen <jon@jonkr.dk>
2022-11-06 08:35:45 +0100

    Simple Insert

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,5 +3,15 @@ using Domain;
 
 FootballLeagueDbContext context = new FootballLeagueDbContext();
 
-context.Leagues.Add(new League { Name = "Red Stripe Premiere League" });
+// context.Leagues.Add(new League { Name = "Red Stripe Premiere League" });
+
+var league = new League { Name = "Liga 2" };
+var team1 = new Team { Name = "Hold 2", League = league };
+var team2 = new Team { Name = "Hold 3", League = league };
+var team3 = new Team { Name = "Hold 4", League = league };
+
+context.Add(team1);
+context.Add(team2);
+context.Add(team3);
+
 context.SaveChanges();
