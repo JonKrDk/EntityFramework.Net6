6d20ddfe34ca91bec15fb670e811bfaae52ee100
Jon Kristensen <jon@jonkr.dk>
2022-11-06 09:57:18 +0100

    Simple Select

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,13 +5,20 @@ FootballLeagueDbContext context = new FootballLeagueDbContext();
 
 // context.Leagues.Add(new League { Name = "Red Stripe Premiere League" });
 
-var league = new League { Name = "Liga 2" };
-var team1 = new Team { Name = "Hold 2", League = league };
-var team2 = new Team { Name = "Hold 3", League = league };
-var team3 = new Team { Name = "Hold 4", League = league };
+//var league = new League { Name = "Liga 2" };
+//var team1 = new Team { Name = "Hold 2", League = league };
+//var team2 = new Team { Name = "Hold 3", League = league };
+//var team3 = new Team { Name = "Hold 4", League = league };
 
-context.Add(team1);
-context.Add(team2);
-context.Add(team3);
+//context.Add(team1);
+//context.Add(team2);
+//context.Add(team3);
 
-context.SaveChanges();
+//context.SaveChanges();
+
+var leagues = context.Leagues.ToList();
+
+foreach (var league in leagues)
+{
+    Console.WriteLine($"{league.Id} {league.Name}");
+}
